416f8a0204695cfe07c4dbfb57873561fcab00b1
artiom <artem@MacBook-Pro-Artem.local>
2019-03-04 16:11:47 +0200

    LESSON10 Home Work

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+var age = 27;
+console.log("my age: ", age);
+console.log("age has type: ", typeof age);
+
+var name = "artiom";
+console.log("my name: ", name);
+console.log("name has type: ", typeof name);
+
+var beautiful = true;
+console.log("I am beautiful ", );
+console.log("beautiful has type: ", typeof beautiful);
+
+var x;
+console.log("x value: ", x);
+console.log("x has type: ", typeof x);
+
+var y = null;
+console.log("y value: ", y);
+console.log("y has type: ", typeof y);
+
+var person = {
+  name: "artiom",
+  age: "27",
+  beautiful: true
+};
+console.log("person value: ", person);
+console.log("person has type: ", typeof person);
+
+var myCurrentAge = 27;
+
+if (myCurrentAge => 25) {
+    console.log("I am over 25 years old");
+} else if (myCurrentAge <= 25) {
+    console.log("I am under 25");
+}
+
+var myCurrentAge = myCurrentAge ? "I am over 25 years old" : "I am under 25";
+    console.log("myCurrentAge: ", myCurrentAge);
